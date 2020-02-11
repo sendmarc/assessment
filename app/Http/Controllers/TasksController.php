3789234aeb035df1a420b1cbb6c76f1dd69f048e
tinkam2020 <60831162+tinkam2020@users.noreply.github.com>
2020-02-11 06:20:38 +0200

    First commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        // load the view and pass the tasks
+        return View::make('tasks.index')
+            ->with('tasks', $tasks);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return View::make('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newTask = $request->only([
+            'name',
+            'priority',
+            'due_in'
+        ]);
+        $task = Task::create($newTask);
+        $task->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        $displaySingle = Task::find($task->id);
+
+        return View::make('tasks.show')
+            ->with('task', $displaySingle);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $changes = $request->only([
+            'name',
+            'priority',
+            'due_in'
+        ]);
+        $task->update($changes);
+        $task->save();
+        return 'tick';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return 'deleted';
+    }
+}
