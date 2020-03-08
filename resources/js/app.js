45fcd2eeaa771a30f0c63c514804f91086a6b769
Ntiyiso Rikhotso <ntiyiso.rikhotso@wunderman.com>
2020-03-08 14:50:55 +0200

    Added initial vue app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import BootstrapVue from 'bootstrap-vue'
+import Vue from 'vue';
+import TasksList from './components/TasksList';
+import TaskRow from './components/TaskRow';
+import Navigation from './components/Navigation';
+import Thead from './components/Thead';
 require('./bootstrap');
+window.$ = require('jquery');
+
+Vue.use(BootstrapVue);
+
+Vue.component('tasks-list', TasksList);
+Vue.component('task-row', TaskRow);
+Vue.component('table-head', Thead);
+Vue.component('navigation', Navigation);
+const app = new Vue({
+    el: '#app',
+    template : '' ,
+});
+
