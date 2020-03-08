e184d62cbebfa4a0c2b1cab57fa941674d011396
Ntiyiso Rikhotso <ntiyiso.rikhotso@wunderman.com>
2020-03-08 14:50:31 +0200

    Add axious default header for csrf token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,14 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//Add headers to axios
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
