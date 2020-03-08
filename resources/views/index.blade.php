a2f626b712a846a403a726d31747ef7376d8b179
Ntiyiso Rikhotso <ntiyiso.rikhotso@wunderman.com>
2020-03-08 14:48:50 +0200

    add view template(blade)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>SendMarc</title>
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <tasks-list :tasks="{{ $tasks }}">
+            </tasks-list>
+
+        </div>
+    </body>
+    <script src="{{ asset('/js/app.js') }}"></script>
+</html>
