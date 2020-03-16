5f166bf70026df5f866b28d291c72f5c0e4ac7f8
Christiaan Nel <guydelta@gmail.com>
2020-03-16 16:47:07 +0200

    Properly bound model to route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\TaskFighter;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -24,7 +25,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
+        Route::model('task', TaskFighter::class);
         parent::boot();
     }
 
--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('tasks', 'TaskController')->only(['index','store','destroy', /*optional?*/ 'show', 'update']);
+Route::apiResource('tasks', 'TaskController')->only(['index','store','destroy', /*optional?*/ 'show', 'update']);
 Route::get('tasks', 'TaskController@index');
-Route::get('tasks/{id}', 'TaskController@show');
+Route::get('tasks/{task}', 'TaskController@show');
 Route::post('tasks', 'TaskController@store');
-Route::put('tasks/{id}', 'TaskController@update');
-Route::delete('tasks/{id}', 'TaskController@destroy');
+Route::put('tasks/{task}', 'TaskController@update');
+Route::delete('tasks/{task}', 'TaskController@destroy');
 
 Route::get('list/tick', 'UpdateTicks');
