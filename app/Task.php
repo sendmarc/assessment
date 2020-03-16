a649b887354893c330c565798d9665675ee7ac63
Christiaan Nel <guydelta@gmail.com>
2020-03-16 23:02:37 +0200

    Refactored Task class

--- app/Task.php
+++ app/Task.php
@@ -13,50 +13,64 @@ class Task extends Model
 //    public $priority;
 //    public $dueIn;
 
+    private static function changePriority($priority, $dueIn, $modifier = 1)
+    {
+        $priority += $modifier * ($dueIn <= 0 ? 2 : 1);
+        if ($priority > 100) return 100;
+        else if ($priority < 0) return 0;
+        else return $priority;
+    }
+
+    private static function changeDueIn($dueIn, $modifier = -1)
+    {
+        $dueIn += $modifier;
+        return $dueIn;
+    }
+
+    private function tickDefault()
+    {
+        $this->priority = self::changePriority($this->priority, $this->dueIn);
+        $this->dueIn = self::changeDueIn($this->dueIn);
+    }
+
+    private function tickGetOlder()
+    {
+        $this->priority = self::changePriority($this->priority, $this->dueIn, -1);
+        $this->dueIn = self::changeDueIn($this->dueIn);
+    }
+
+    private function tickCompleteAssessment()
+    {
+        if ($this->dueIn > 10) {
+            $this->priority = self::changePriority($this->priority, 1, 1);
+        } else if ($this->dueIn > 5) {
+            $this->priority = self::changePriority($this->priority, 1, 2);
+        } else if ($this->dueIn > 0 ) {
+            $this->priority = self::changePriority($this->priority, 1, 3);
+        } else {
+            $this->priority = 0;
+        }
+        $this->dueIn = self::changeDueIn($this->dueIn);
+    }
+
+    private function tickBreathe()
+    {
+        //Change nothing on breathe
+        return;
+    }
+
+
     public function tick()
     {
-        if ($this->name != 'Get Older') {
-            if ($this->priority < 100) {
-                if ($this->name != 'Breathe') {
-                    $this->priority = $this->priority + 1;
-                }
-            }
-            if ($this->name == 'Complete Assessment') {
-                if ($this->dueIn < 11) {
-                    if ($this->priority < 100) {
-                        $this->priority = $this->priority + 1;
-                    }
-                }
-                if ($this->dueIn < 6) {
-                    if ($this->priority < 100) {
-                        $this->priority = $this->priority + 1;
-                    }
-                }
-            }
-        } else {
-            if ($this->priority > 0) {
-                $this->priority = $this->priority - 1;
-            }
-        }
-        if ($this->name != 'Breathe') {
-            $this->dueIn = $this->dueIn - 1;
-        }
-        if ($this->dueIn < 0) {
-            if ($this->name != 'Get Older') {
-                if ($this->name != 'Complete Assessment') {
-                    if ($this->priority < 100) {
-                        if ($this->name != 'Breathe') {
-                            $this->priority = $this->priority + 1;
-                        }
-                    }
-                } else {
-                    $this->priority = $this->priority - $this->priority;
-                }
-            } else {
-                if ($this->priority > 0) {
-                    $this->priority = $this->priority - 1;
-                }
-            }
+        switch ($this->name) {
+            case "Get Older":
+                return $this->tickGetOlder();
+            case "Complete Assessment":
+                return $this->tickCompleteAssessment();
+            case "Breathe":
+                return $this->tickBreathe();
+            default:
+                return $this->tickDefault();
         }
     }
 }
