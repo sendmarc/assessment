d9703e693b1a23b9e46690b30ef11321ac043e3f
Christiaan Nel <guydelta@gmail.com>
2020-03-16 23:02:23 +0200

    Completed unit tests

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -3,99 +3,152 @@
 namespace Tests\Unit;
 
 use App\Task;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
-    use RefreshDatabase;
-
     public function testItImplementsTheModelClass()
     {
-
+        self::assertInstanceOf(Model::class, new Task());
     }
 
     public function testItCanGetInstanceOfTask()
     {
-
+        self::assertInstanceOf(Task::class, new Task());
     }
 
     public function testItDoesntIncreasePriorityOverHundred()
     {
-        //Add task with priority 99
-        //Tick twice
-        //Check priority = 100
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Test', 'priority' => 99, 'dueIn' => 100]);
+        $task->tick();
+        self::assertEquals(100, $task->priority);
+        $task->tick();
+        self::assertEquals(100, $task->priority);
+        $task->delete();
     }
 
     public function testItDoesntDecreasePriorityBelowZero()
     {
-        //Retrieve get older task
-        //Change priority to 1
-        //Tick twice
-        //Check priority = 0;
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Get Older', 'priority' => 1, 'dueIn' => 100]);
+        $task->tick();
+        self::assertEquals(0, $task->priority);
+        $task->tick();
+        self::assertEquals(0, $task->priority);
+        $task->tick();
+        self::assertEquals(0, $task->priority);
+        $task->delete();
     }
 
     public function testItIncreasePriorityOnTick()
     {
-        //Create task
-        //Tick
-        //Check priority
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Test', 'priority' => 50, 'dueIn' => 100]);
+        $task->tick();
+        self::assertEquals(51, $task->priority);
+        $task->tick();
+        self::assertEquals(52, $task->priority);
+        $task->delete();
     }
 
     public function testItDecreaseDueOnTick()
     {
-        //Create task
-        //Tick
-        //Check due
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Test', 'priority' => 50, 'dueIn' => 100]);
+        $task->tick();
+        self::assertEquals(99, $task->dueIn);
+        $task->tick();
+        self::assertEquals(98, $task->dueIn);
+        $task->delete();
     }
 
     public function testItDoublePriorityAfterDueOnTick()
     {
-        //Create task with due = 1 and priority = 0
-        //Tick
-        //due should 0 and priority should 1
-        //Tick
-        //due should -1 and priority should 3
-        //Tick
-        //due should -2 and priority should 5
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Test', 'priority' => 0, 'dueIn' => 1]);
+        $task->tick();
+        self::assertEquals(0, $task->dueIn);
+        self::assertEquals(1, $task->priority);
+        $task->tick();
+        self::assertEquals(-1, $task->dueIn);
+        self::assertEquals(3, $task->priority);
+        $task->tick();
+        self::assertEquals(-2, $task->dueIn);
+        self::assertEquals(5, $task->priority);
+        $task->delete();
     }
 
     public function testItDoesntUpdateBreatheTaskOnTick()
     {
-        //Get breathe task
-        //Tick
-        //Check breathe task values
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Breathe', 'priority' => 1000, 'dueIn' => 30]);
+        $task->tick();
+        self::assertEquals(30, $task->dueIn);
+        self::assertEquals(1000, $task->priority);
+        $task->tick();
+        self::assertEquals(30, $task->dueIn);
+        self::assertEquals(1000, $task->priority);
+        $task->tick();
+        self::assertEquals(30, $task->dueIn);
+        self::assertEquals(1000, $task->priority);
+        $task->delete();
     }
 
    public function testItHasNegativePriorityForGetOlderTaskOnTick()
    {
-       //Get get older task
-       //Set due in to 1
-       //Tick
-       //Priority should decrease
-       //Tick
-       //Priority should deacrease by 2
+       /** @var Task $task */
+       $task = Task::create(['name' => 'Get Older', 'priority' => 6, 'dueIn' => 2]);
+       $task->tick();
+       self::assertEquals(1, $task->dueIn);
+       self::assertEquals(5, $task->priority);
+       $task->tick();
+       self::assertEquals(0, $task->dueIn);
+       self::assertEquals(4, $task->priority);
+       $task->tick();
+       self::assertEquals(-1, $task->dueIn);
+       self::assertEquals(2, $task->priority);
+       $task->tick();
+       self::assertEquals(-2, $task->dueIn);
+       self::assertEquals(0, $task->priority);
+       $task->tick();
+       self::assertEquals(-3, $task->dueIn);
+       self::assertEquals(0, $task->priority);
+       $task->delete();
    }
 
     public function testItChangesPriorityOfCompleteAssessmentTaskOnTick()
     {
-        //Get assessment task
-        //Set due = 11; priority = 50
-        //Tick
-        //Check changed by one
-        //Tick
-        //Check changed by 2
-        //Tick * 4
-        //Check changed by 8
-        //Tick
-        //Check changed by 3
-        //Tick * 4
-        //Check change by 12
-        //Tick
-        //Check priority is 0
-        //Tick
-        //Check priority remains at 0
-
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Complete Assessment', 'priority' => 50, 'dueIn' => 11]);
+        $task->tick();
+        self::assertEquals(51, $task->priority);
+        self::assertEquals(10, $task->dueIn);
+        $task->tick();
+        self::assertEquals(53, $task->priority);
+        self::assertEquals(9, $task->dueIn);
+        for($i = 0; $i < 4; $i++) {
+            $task->tick();
+        }
+        self::assertEquals(61, $task->priority);
+        self::assertEquals(5, $task->dueIn);
+        $task->tick();
+        self::assertEquals(64, $task->priority);
+        self::assertEquals(4, $task->dueIn);
+        for($i = 0; $i < 4; $i++) {
+            $task->tick();
+        }
+        self::assertEquals(76, $task->priority);
+        self::assertEquals(0, $task->dueIn);
+        $task->tick();
+        self::assertEquals(0, $task->priority);
+        self::assertEquals(-1, $task->dueIn);
+        $task->tick();
+        self::assertEquals(0, $task->priority);
+        self::assertEquals(-2, $task->dueIn);
+        $task->delete();
     }
 
     /*
@@ -107,27 +160,39 @@ class TaskTest extends TestCase
      */
     public function testItCanAddNewTask()
     {
-
+        /** @var Task $task */
+        $task = Task::create(['name' => 'Test', 'priority' => 50, 'dueIn' => 100]);
+        self::assertNotNull($task);
         return $task;
     }
 
     /** @depends testItCanAddNewTask */
     public function testItCanGetATask(Task $task)
     {
-
+        /** @var Task $findTask */
+        $findTask = Task::find($task->id);
+        self::assertSame($task->id, $findTask->id);
         return $task;
     }
 
-    /** @depends testItCanAddNewTask */
+    /** @depends testItCanGetATask */
     public function testItCanUpdateTask(Task $task)
     {
-
+        self::assertSame("Test", $task->name);
+        $task->name = "Test 2";
+        $task->save();
+        $findTask = Task::find($task->id);
+        self::assertSame("Test 2", $task->name);
+        self::assertSame("Test 2", $findTask->name);
         return $task;
     }
 
     /** @depends testItCanUpdateTask */
     public function testItCanDeleteTask(Task $task)
     {
-
+        $id = $task->id;
+        $task->delete();
+        $findTask = Task::find($id);
+        self::assertNull($findTask);
     }
 }
