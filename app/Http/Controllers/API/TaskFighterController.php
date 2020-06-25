f5045ea8ba06ec053d73c695c31d353fe31a0c05
Kgolofelo Selowa <kgolofelo@agileroutes.com>
2020-06-25 15:32:13 +0200

    Added a method with some rules to save a task.

--- app/Http/Controllers/API/TaskFighterController.php
+++ app/Http/Controllers/API/TaskFighterController.php
@@ -7,8 +7,29 @@ use App\Task;
 
 class TaskFighterController extends Controller
 {
+    private $rules = [
+        'name' => 'required',
+        'priority' => 'required|integer|between:0,100',
+        'dueIn' => 'required|integer'
+    ];
+
     public function index()
     {
         return Task::all();
+    }
+
+    /**
+     * Save a new task. Prevents saving the same task more than once.
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function store()
+    {
+        try {
+            $data = request()->validate($this->rules);
+            return Task::firstOrCreate($data);
+        } catch (\Exception $exception) {
+            return response($exception->getMessage(), 500);
+        }
     }
 }
