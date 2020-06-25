71984751df3b1cc6d401a12c4c07328cf1b99bb1
Kgolofelo Selowa <kgolofelo@agileroutes.com>
2020-06-25 22:32:13 +0200

    Added task tick api route.

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResources(['tasks' => 'API\TaskFighterController']);
+Route::get('list/tick', 'API\TaskFighterController@tick');
