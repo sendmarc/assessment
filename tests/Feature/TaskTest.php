8c30e0a852d5040b4bfc147a69f3ef68ac78fd73
Kgolofelo Selowa <kgolofelo@agileroutes.com>
2020-06-25 15:29:57 +0200

    Test case for adding a task.

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -6,10 +6,28 @@ use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
+    protected $taskStructure = ['name', 'priority', 'dueIn'];
+    protected $taskData = [
+        'name' => 'KG',
+        'priority' => 4,
+        'dueIn' => 9
+    ];
+
     public function testGetTaskList()
     {
         $this->withoutExceptionHandling();
         $response = $this->get('/api/tasks');
         $response->assertOk();
+    }
+
+    public function testTaskSave()
+    {
+        $this->refreshApplication();
+        $response = $this->postTask();
+        $response->assertJsonStructure($this->taskStructure);
+    }
+
+    private function postTask() {
+        return $this->post('/api/tasks', $this->taskData);
     }
 }
