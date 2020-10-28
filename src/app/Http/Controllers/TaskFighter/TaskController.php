ff7fd0dcc329dd78044edf2f2ae31db88059bbf2
Mpilo Cut-one <mpilokhathwane@gmail.com>
2020-10-28 14:49:15 +0200

    Task1: Changes routes to use Controllers, Requests and Models

---
+++ src/app/Http/Controllers/TaskFighter/TaskController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\TaskFighter;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\TaskFighter;
+
+
+class TaskController extends Controller
+{
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        $tasks = Task::paginate(5);
+        return $tasks;
+    }
+
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        $task = new Task();
+        $task->name = $request->name;
+        $task->priority = $request->priority;
+        $task->dueIn = $request->dueIn;
+        $task->save();
+
+        return 'Created!';
+    }
+
+    /**
+    * Get the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return $task;
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+
+        if ($request->has('name')) $task->name = $request->name;
+        if ($request->has('priority')) $task->priority = $request->priority;
+        if ($request->has('dueIn')) $task->dueIn = $request->dueIn;
+
+        $task->save();
+
+        return 'Updated!';
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return 'Deleted!';
+    }
+
+    /**
+    * Cause a tick
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function tick()
+    {
+        $tasks = Task::all();
+        foreach ($tasks as $task) {
+            $taskFighter = new \App\TaskFighter($task->name,$task->priority,$task->dueIn);
+            $taskFighter->tick();
+
+            $request = new \Illuminate\Http\Request([
+                'id' => $task->id,
+                'priority' => $taskFighter->priority,
+                'dueIn' => $taskFighter->dueIn,
+            ]);
+
+            $this->update($request, $task->id);
+        }
+
+        return 'Done!';
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\TaskFighter\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tasks', [TaskController::class, 'index']);
+
+Route::get('/list/tick', [TaskController::class, 'tick']);
+
+Route::post('/tasks', [TaskController::class, 'store']);
+
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
