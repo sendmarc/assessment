67f8ee1b7381125118ea848e7a8006df31b819d0
Etienne Du Plessis <etiennedp@moredigital.co.za>
2020-12-10 07:01:00 +0200

    Fixed phpunit.xml - now uses sqlite and memory Started refactoring TaskFighter.php

--- app/TaskFighter.php
+++ app/TaskFighter.php
@@ -4,65 +4,85 @@ namespace App;
 
 class TaskFighter
 {
+    /** @var string */
     public $name;
 
+    /** @var int */
     public $priority;
 
+    /** @var int */
     public $dueIn;
 
-    public function __construct($name, $priority, $due_in)
+    /** @var int */
+    private $priorityIncrement = 1;
+
+    /** @var int */
+    private $priorityMax = 100;
+
+    /** @var int */
+    private $priorityMin = 0;
+
+    /** @var int */
+    private $priorityWarn = 10;
+
+    /** @var int */
+    private $priorityDanger = 5;
+
+    /**
+     * TaskFighter constructor.
+     * @param string $name
+     * @param int $priority
+     * @param int $due_in
+     */
+    public function __construct(string $name, int $priority, int $due_in)
     {
         $this->name = $name;
         $this->priority = $priority;
         $this->dueIn = $due_in;
     }
 
-    public static function of($name, $priority, $dueIn) {
-        return new static($name, $priority, $dueIn);
-    }
-
     public function tick()
     {
-        if ($this->name != 'Get Older') {
-            if ($this->priority < 100) {
-                if ($this->name != 'Breathe') {
-                    $this->priority = $this->priority + 1;
+        if ($this->name !== 'Get Older') {
+            if ($this->priority < $this->priorityMax) {
+                if ($this->name !== 'Breathe') {
+                    $this->priority = $this->priority + $this->priorityIncrement;
                 }
             }
-            if ($this->name == 'Complete Assessment') {
-                if ($this->dueIn < 11) {
-                    if ($this->priority < 100) {
-                        $this->priority = $this->priority + 1;
+            if ($this->name === 'Complete Assessment') {
+                if ($this->dueIn <= $this->priorityWarn) {
+                    if ($this->priority < $this->priorityMax) {
+                        $this->priority = $this->priority + $this->priorityIncrement;
                     }
                 }
-                if ($this->dueIn < 6) {
-                    if ($this->priority < 100) {
-                        $this->priority = $this->priority + 1;
+                if ($this->dueIn <= $this->priorityDanger) {
+                    if ($this->priority < $this->priorityMax) {
+                        $this->priority = $this->priority + $this->priorityIncrement;
                     }
                 }
             }
         } else {
-            if ($this->priority > 0) {
-                $this->priority = $this->priority - 1;
+            if ($this->priority > $this->priorityMin) {
+                $this->priority = $this->priority - $this->priorityIncrement;
             }
         }
-        if ($this->name != 'Breathe') {
-            $this->dueIn = $this->dueIn - 1;
+        if ($this->name !== 'Breathe') {
+            $this->dueIn = $this->dueIn - $this->priorityIncrement;
         }
-        if ($this->dueIn < 0) {
-            if ($this->name != 'Get Older') {
-                if ($this->name != 'Complete Assessment') {
-                    if ($this->priority < 100) {
+        if ($this->dueIn < $this->priorityMin) {
+            if ($this->name !== 'Get Older') {
+                if ($this->name !== 'Complete Assessment') {
+                    if ($this->priority < $this->priorityMax) {
                         if ($this->name != 'Breathe') {
-                            $this->priority = $this->priority + 1;
+                            $this->priority = $this->priority + $this->priorityIncrement;
                         }
                     }
                 } else {
                     $this->priority = $this->priority - $this->priority;
                 }
             } else {
-                if ($this->priority > 0) {
-                    $this->priority = $this->priority - 1;
+                if ($this->priority > $this->priorityMin) {
+                    $this->priority = $this->priority - $this->priorityIncrement;
                 }
             }
         }
